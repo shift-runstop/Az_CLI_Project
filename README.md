eb97f6970cf77aaa4621e1a21c20a1cb324379fa
shift-runstop <shift-runstop@protonmail.ch>
2023-09-15 13:41:53 +0100

    README update, almost fixed

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-# Az_CLI
+---
+id: "904dfba4-d813-4134-9102-57d48af9c009"
+title: "README"
+source: ""
+aliases: ["README"]
+---
+This is a python script to allow you to communicate with your [Personal AI](https://personal.ai) via CLI.
 
-TODO
+# Installation
+* Clone the repository
+```
+git clone https://github.com/shift-runstop/Az_CLI_Project/blob/main/test.py
+```
+* Create a .env file in the same folder with the following information:
+```
+API_KEY=
+BASE_URL=https://api.personal.ai/v1/message
+```
+* Put your PAI api key in the appropriate field
 
-- [ ] Start python3 scriptname message > sends to my AI > AI outputs to CLI
-
+# Usage
+python3 scriptname.py Your Message Here.
--- test.py
+++ test.py
@@ -4,35 +4,25 @@ import os
 import sys
 
 api_key = os.getenv('API_KEY')
-base_url = os.getenv('BASE_URL', 'https://api.personal.ai/v1/message')
-memory_api_url = os.getenv('MEMORY_API_URL', 'https://api.personal.ai/v1/memory')
+base_url = 'https://api.personal.ai/v1/message'
 
-def send_ai_message(api_key, message):
+def send_ai_message():
     headers = {
         'Content-Type': 'application/json',
         'x-api-key': api_key
     }
 
-    payload = {"Text": message}
+    payload = json.dumps({
+                "Text": "Hello"
+    })
+
     print(payload)
-    response = requests.request("POST", base_url, headers=headers, json=payload)
-    print("Gimme a 200" ,response)
-    if response.status_code == 200:
-        ai_response = response.json()
-        print("ok you run now", ai_response)
-        return ai_response
-    else:
-        return None
+    response = requests.request("POST", base_url, headers=headers, data=payload)
+    print(response.text)
 
 def main():
     message = sys.argv[1:]
-    ai_response = send_ai_message(api_key, message)
-
-    print("main", ai_response)
-    if ai_response is not None:
-        print(f"AI Response: {ai_response}")
-    else:
-        print("Error sending message to AI")
+    send_ai_message()
 
 if __name__ == "__main__":
     main()
