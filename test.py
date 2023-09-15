85c7b4f0b353fe677a5f2566baeaa08c76dc9dff
shift-runstop <shift-runstop@protonmail.ch>
2023-09-15 14:27:35 +0100

    up and running with shebang

--- test.py
+++ test.py
@@ -1,28 +1,33 @@
+#! /usr/bin/env/python3
+
 import requests
 import json
 import os
 import sys
+from dotenv import load_dotenv
 
+load_dotenv()
 api_key = os.getenv('API_KEY')
-base_url = 'https://api.personal.ai/v1/message'
+url = 'https://api.personal.ai/v1/message'
 
-def send_ai_message():
+def send_ai_message(api_key, message):
     headers = {
         'Content-Type': 'application/json',
         'x-api-key': api_key
     }
 
     payload = json.dumps({
-                "Text": "Hello"
+        "Text": message
     })
 
     print(payload)
-    response = requests.request("POST", base_url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
     print(response.text)
 
 def main():
-    message = sys.argv[1:]
-    send_ai_message()
+    message = sys.argv[1]
+    send_ai_message(api_key, message)
 
 if __name__ == "__main__":
     main()
+
