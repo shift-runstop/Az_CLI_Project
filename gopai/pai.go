1e7f3896ef79cfc47536e6b0105babf17b84f9b1
shift-runstop <shift-runstop@protonmail.ch>
2023-09-18 14:36:59 +0100

    Debugging Go, on wrong branch, work from here

--- gopai/pai.go
+++ gopai/pai.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/joho/godotenv"
 )
 
 const url = "https://api.personal.ai/v1/message"
@@ -15,7 +17,7 @@ type Payload struct {
 	Text string `json:"Text"`
 }
 
-func sendAIMessage(apiKey string, message string) {
+func sendAIMessage(apiKey string, message string) string {
 	headers := make(map[string]string)
 	headers["Content-Type"] = "application/json"
 	headers["x-api-key"] = apiKey
@@ -23,28 +25,38 @@ func sendAIMessage(apiKey string, message string) {
 	payload := &Payload{Text: message}
 	payloadBytes, err := json.Marshal(payload)
 	if err != nil {
-		fmt.Println("Error marshalling payload:", err)
-		return
+		return "Error marshalling payload:" + err.Error()
 	}
 
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(payloadBytes))
 	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return
+		return "Error sending request:" + err.Error()
 	}
 	defer resp.Body.Close()
 
 	var responseData map[string]interface{}
 	if err := json.NewDecoder(resp.Body).Decode(&responseData); err != nil {
-		fmt.Println("Error decoding response:", err)
-		return
+		return "Error decoding response:" + err.Error()
 	}
 
-	fmt.Println()
+	fmt.Println("API RESPONSE:", responseData)
+
+	if aiMessage, ok := responseData["ai_message"].(string); ok {
+		return aiMessage
+	}
+
 	fmt.Println(responseData["ai_message"])
+
+	return "<nil?>"
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(".env not loaded")
+		return
+	}
+
 	apiKey := os.Getenv("API_KEY")
 	if apiKey == "" {
 		fmt.Println("API_KEY not set in environment")
