a6e57b81855c2227cd78d18a56332dd3175973f4
sRuxen <seancruz2021@protonmail.com>
2023-09-19 02:26:58 +1000

    originally i did a much more comprehensive list but I accidently deleted it unfortunatly.
    --------------------
    
    mild code rework
    additon of stack_memory() - not working due to http error 500
    error prevention to fix common errors related to missing or
    incorrect api key or lack of message input
    
    --------------------
    
    need to fix memory stacking still. trying to stack memorys
    from the api is throwing a HTTP error <500> (Internal Server Error)
    
    want to look into also autocreation of .env file if it is nonexistant.
    to make end user usage and setup easier, but not a priority.
    
    want to clean code up a bit more. so far im relativly satisfied, especially
    with my abuse of comments (mainly because they make it easier
    for anyone, including those less familiar with python, to read and
    understand the code)

--- pai_test.py
+++ pai_test.py
@@ -1,1 +1,94 @@
 
+#! /usr/bin/env/python3
+
+#SETUP======================================================#
+
+import json
+import os
+import sys
+
+#pip install requests
+import requests 
+
+#pip install python-dotenv
+from dotenv import load_dotenv 
+
+
+#MAIN SCRIPT================================================#
+
+#get api key from .env
+load_dotenv()
+api_key = os.getenv('API_KEY') 
+
+# define urls for message and memory
+message_url = 'https://api.personal.ai/v1/message'
+memory_url = 'https://api.personal.ai/v1/memory'
+
+def concat(args, sep=" "): 
+    #return concatinaton of arguments if there is 1 or more (error prevention)
+    if len(args) >= 1: 
+        return sep.join(args)
+    
+def send_message(message):
+    headers = {
+        'Content-Type': 'application/json',
+        'x-api-key': api_key
+    }
+    payload = json.dumps({ #json.dumps to ensure json format is sent as *requried*
+        "Text": message #message (string data) to send AI.
+    })
+
+    #send message to AI.
+    response = requests.request("POST", message_url, headers=headers, data=payload)
+    response_data = response.json()
+
+    try:
+        return response_data['ai_message']
+    except: 
+        "unable to fetch message response"
+
+def stack_memory(text):
+    headers = {
+        'Content-Type': 'application/json',
+        'x-api-key': api_key
+    }
+
+    payload = json.dumps({ #json.dumps to ensure json format is sent as *requried*
+        "Text": text, #text data to stack to AI's memory.
+        "SourceName": "CLI", #source name *required* by api.
+    })
+
+    #send memory data to server
+    response = requests.request("POST", memory_url, headers=headers, data=payload, timeout=60)
+    
+    #print response (debug purpouses)
+    print(response)
+    
+
+        
+def main(): #this is where we will bring it together
+
+    cli_input = sys.argv[1:] #get arguments for message
+    message = concat(cli_input) #concat into message string or none
+
+    if message: #if there is a message to send to the ai
+        
+        #send message to ai. store and print response.
+        response = str(send_message(message))
+        print("\n " + str(response))
+
+        #stack message sent to ai and ai response.
+        stack_memory(
+            "recieved message: " + message +
+            "\n AI response:" + response)
+            #!!!!currently returns error 500, internal server error.!!!!
+
+#if there is an api key, run main (script bugs out end errors without it
+# due to lack of key in line 36)
+if len(api_key) > 0:
+    main()
+else:
+    #if there isnt a key, inform user key is missing.
+    print("api key missing") 
+    #-- refine later to inform of .env file
+    #-- possibly auto create .env file ?
