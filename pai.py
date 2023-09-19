802ca8c2dc4bed25bcdbeeda0df8446fc5a46806
sRuxen <seancruz2021@protonmail.com>
2023-09-20 05:26:42 +1000

    soft readability changes to do with comments.

--- pai.py
+++ pai.py
@@ -1,16 +1,16 @@
 
 #! /usr/bin/env/python3
 
-#SETUP======================================================#
+#IMPORTS======================================================#
 
 import json
 import os
 import argparse
 
-#pip install requests
-import requests 
+import requests #pip install requests
 
-#load config.json
+#CONFIG=======================================================#
+
 default_config = {
 'api_key' : None,
 'UserName': None,
@@ -18,6 +18,7 @@ default_config = {
 'DomainName_stackMessage':None,
 'DomainName_stackResponse':None,
 }
+
 def load_config(name="config.json"):
     try:
         f = open(name, "r")
@@ -37,28 +38,30 @@ def save_config():
     f.write(json.dumps(config, indent=4))
     f.close()    
 
-#==========ARGUMENTS
+#ARGUMENTS====================================================#
+
+#define argument parser thing.
 parser = argparse.ArgumentParser(
             prog='PAI Command Line Interface',
             description='A tool for interaction with PAI AI from the command line',
             epilog='**Work In Progress**')
 
 #define arguments
-
 parser.add_argument('-s', '-stack', dest='stack', action='store_true', help='stack message and ai response') #optional true or false argument
 parser.add_argument('-c', '--config', metavar='CONFIG FILE', help='for loading a config that is not the default. must be .json file.') #value argument
 parser.add_argument('-key', dest='api_key', metavar='API KEY') #value argument
 parser.add_argument('message', nargs='*', help='type message to AI after arguments') #positional argument : 'message'. nargs='*' ensures we get same usable output as sys.argv[1:]
 
-
 #set parsed arguments as variable.
 args = parser.parse_args()
 
-#value argument handeling
-if args.api_key:
+#handle argument values acordingly
+
+if args.api_key: # -key
     config['api_key'] = args.api_key
     save_config()
-if args.config:
+
+if args.config: # -c --config
     config = load_config(args.config)
 
 #=========CONSTANT VARIABLE DEFINITIONS
@@ -68,12 +71,14 @@ message_url = 'https://api.personal.ai/v1/message'
 memory_url = 'https://api.personal.ai/v1/memory'
 
 #MAIN SCRIPT================================================#
+
+#return concatinaton arguments if there is more than 1
 def concat(args, sep=" "): 
-    #return concatinaton of arguments if there is 1 or more (error prevention)
     if len(args) >= 1: 
         return sep.join(args)
-    
-def send_message(message): #send message to AI function
+
+#send message to AI 
+def send_message(message): 
     headers = {
         'Content-Type': 'application/json',
         'x-api-key': api_key,
@@ -84,7 +89,7 @@ def send_message(message): #send message to AI function
         'UserName':config['UserName'],
     })
 
-    #send message to AI.
+    #actual sending of payload (message) to AI via API.
     response = requests.request("POST", message_url, headers=headers, data=payload)
     response_data = response.json()
 
@@ -93,6 +98,7 @@ def send_message(message): #send message to AI function
     except: 
         "unable to fetch message response"
 
+#save memory to stack. use same domain as sending to unless otherwise defined on function call.
 def stack_memory(text, domain=config['DomainName_sendMessage']): #stack memory to AI
     headers = {
         'Content-Type': 'application/json',
@@ -108,23 +114,26 @@ def stack_memory(text, domain=config['DomainName_sendMessage']): #stack memory t
     #send memory data to server
     response = requests.request("POST", memory_url, headers=headers, data=payload, timeout=60)
 
-        
-def main(): #this is where we will bring it together
-    message = concat(args.message) #concat into message string or none
+
+#this is where we will bring it together
+def main(): 
+    message = concat(args.message) #concatinate list into usable message string
 
     if message: #if there is a message to send to the ai
         
-        #send message to ai. store and print response.
+        #send message to ai. store the response and the name of the ai that responds
         ai_name, response = send_message(message)
 
         print("\n" + str(ai_name) + " > " + str(response) + "\n")
 
         #stack message sent to ai and ai response if stack is set to true (see arguments)
         if args.stack:
+            #for sending to 2 diffent domains
             if config['DomainName_stackMessage'] != config['DomainName_stackResponse']:
                 stack_memory("recieved message: " + str(message), config['DomainName_stackMessage'])
                 stack_memory("AI response: " + str(response), config['DomainName_stackResponse'])
                 print("stacked message to: " + config['DomainName_stackMessage'] + "\nstacked response to: " + config['DomainName_stackResponse'])
+            #for sending to 1 domain
             else:   
                 stack_memory(
                     "recieved message: " + str(message) +
